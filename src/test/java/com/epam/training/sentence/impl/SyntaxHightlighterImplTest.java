cbb728bc7cee67d04970591bf5485e2e27073ef9
Philip Ardley <philard@gmail.com>
2017-03-16 21:11:40 +0800

    Unit test SyntaxHightlighterImplTest#shouldApplyStyleOnThisSentence now works.

--- src/test/java/com/epam/training/sentence/impl/SyntaxHightlighterImplTest.java
+++ src/test/java/com/epam/training/sentence/impl/SyntaxHightlighterImplTest.java
@@ -4,16 +4,13 @@ import com.epam.training.exception.SyntaxHighlightingException;
 import com.epam.training.validation.impl.SentenceValidatorImpl;
 import com.epam.training.word.IWordHighlighter;
 import com.epam.training.word.impl.SimpleWordHighlighter;
-import jdk.nashorn.internal.runtime.regexp.joni.exception.SyntaxException;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
-import org.mockito.stubbing.OngoingStubbing;
 
 import java.util.ArrayList;
 
-import org.mockito.Matchers;
-
+import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.*;
 
 /**
@@ -23,23 +20,35 @@ public class SyntaxHightlighterImplTest {
 
     private SyntaxHighlighterImpl syntaxHighlighter;
 
-    private IWordHighlighter wordHighlighter;
+    private IWordHighlighter amHighlighter;
+
+    private ArrayList<IWordHighlighter> wordHighlighters;
 
     @Before
     public void beforeTest() {
 
         syntaxHighlighter = new SyntaxHighlighterImpl();
-
         syntaxHighlighter.setSentenceValidatorImpl(getSentenceValidator());
 
-
-        wordHighlighter = mock(SimpleWordHighlighter.class);
-        doReturn("I'm going ")
-                .when(wordHighlighter).highlightThis("I'm going ");
-        ArrayList<IWordHighlighter> wordHighlighters = new ArrayList<IWordHighlighter>();
-        wordHighlighters.add(wordHighlighter);
+        wordHighlighters = buildWordHighlighters();
         syntaxHighlighter.setWordHighlighters(wordHighlighters);
+    }
 
+    private ArrayList<IWordHighlighter> buildWordHighlighters() {
+        wordHighlighters = new ArrayList<IWordHighlighter>();
+        amHighlighter = mock(SimpleWordHighlighter.class);
+        when(amHighlighter.highlightThis("I am going to join java mentoring program " +
+                "to learn cool stuff in fun way."))
+                .thenReturn("I [bold]am[/bold] going to join java mentoring program " +
+                        "to learn cool stuff in fun way.");
+        wordHighlighters.add(amHighlighter);
+        SimpleWordHighlighter toHighlighter = mock(SimpleWordHighlighter.class);
+        when(toHighlighter.highlightThis("I [bold]am[/bold] going to join java mentoring program " +
+                "to learn cool stuff in fun way."))
+                .thenReturn("I [bold]am[/bold] going [italic]to[/italic] join java mentoring program " +
+                        "[italic]to[/italic] learn cool stuff in fun way.");
+        wordHighlighters.add(toHighlighter);
+        return wordHighlighters;
     }
 
     private SentenceValidatorImpl getSentenceValidator() {
@@ -74,4 +83,44 @@ public class SyntaxHightlighterImplTest {
         syntaxHighlighter.highlightThis(sentence);
     }
 
+    @Test
+    public void shouldNotApplyStyleOnThisSentence() {
+        String sentence = "I know everything, so it's waste of effort.";
+        String expectedHighlightedSentence = "I know everything, so it's waste of effort.";
+
+        String highlightSentence = syntaxHighlighter.highlightThis(sentence);
+
+        assertEquals(expectedHighlightedSentence, highlightSentence);
+    }
+
+    @Test
+    public void shouldApplyStyleOnThisSentence() {
+        String sentence = "I am going to join java mentoring program to learn cool stuff in fun way.";
+        String expectedHighlightedSentence = "I [bold]am[/bold] going [italic]to[/italic] join java mentoring program " +
+                "[italic]to[/italic] learn cool stuff in fun way.";
+
+        String highlightSentence = syntaxHighlighter.highlightThis(sentence);
+
+        assertEquals(expectedHighlightedSentence, highlightSentence);
+    }
+
+    @Test
+    public void shouldApplyStyleOnlyWhenMatchedWholeWord() {
+        String sentence = "I am going in now.";
+        String expectedHighlightedSentence = "I [bold]am[/bold] going [underline]in[/underline] now.";
+
+        String highlightSentence = syntaxHighlighter.highlightThis(sentence);
+
+        assertEquals(expectedHighlightedSentence, highlightSentence);
+    }
+
+    @Test
+    public void shouldApplyStyleWhenMatchedWordIsNextToAFullStop() {
+        String sentence = "I will go in.";
+        String expectedHighlightedSentence = "I will go [underline]in[/underline].";
+
+        String highlightSentence = syntaxHighlighter.highlightThis(sentence);
+
+        assertEquals(expectedHighlightedSentence, highlightSentence);
+    }
 }
