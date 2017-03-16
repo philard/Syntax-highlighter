d7b3ddd973d5ba5430fbbac7c87a304e0591016c
Philip Ardley <philard@gmail.com>
2017-03-16 20:48:23 +0800

    Unit test for SyntaxHightlighterImpl partially done.

--- src/test/java/com/epam/training/sentence/impl/SyntaxHightlighterImplTest.java
+++ src/test/java/com/epam/training/sentence/impl/SyntaxHightlighterImplTest.java
@@ -2,13 +2,19 @@ package com.epam.training.sentence.impl;
 
 import com.epam.training.exception.SyntaxHighlightingException;
 import com.epam.training.validation.impl.SentenceValidatorImpl;
+import com.epam.training.word.IWordHighlighter;
+import com.epam.training.word.impl.SimpleWordHighlighter;
 import jdk.nashorn.internal.runtime.regexp.joni.exception.SyntaxException;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
 import org.mockito.stubbing.OngoingStubbing;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import java.util.ArrayList;
+
+import org.mockito.Matchers;
+
+import static org.mockito.Mockito.*;
 
 /**
  * Created by Philip on 16/03/2017.
@@ -17,23 +23,36 @@ public class SyntaxHightlighterImplTest {
 
     private SyntaxHighlighterImpl syntaxHighlighter;
 
-    private SentenceValidatorImpl sentenceValidatorImpl;
+    private IWordHighlighter wordHighlighter;
 
     @Before
     public void beforeTest() {
 
         syntaxHighlighter = new SyntaxHighlighterImpl();
-        syntaxHighlighter.setSentenceValidatorImpl(sentenceValidatorImpl);
 
-        sentenceValidatorImpl = mock(SentenceValidatorImpl.class);
+        syntaxHighlighter.setSentenceValidatorImpl(getSentenceValidator());
 
-        OngoingStubbing noValue;
-        noValue = when(sentenceValidatorImpl.validate(null))
-                .thenThrow(SyntaxException.class);
-        when(sentenceValidatorImpl.validate(""))
-                .thenThrow(SyntaxException.class));
-        when(sentenceValidatorImpl.validate("    "))
-                .thenThrow(SyntaxException.class));
+
+        wordHighlighter = mock(SimpleWordHighlighter.class);
+        doReturn("I'm going ")
+                .when(wordHighlighter).highlightThis("I'm going ");
+        ArrayList<IWordHighlighter> wordHighlighters = new ArrayList<IWordHighlighter>();
+        wordHighlighters.add(wordHighlighter);
+        syntaxHighlighter.setWordHighlighters(wordHighlighters);
+
+    }
+
+    private SentenceValidatorImpl getSentenceValidator() {
+        SentenceValidatorImpl sentenceValidatorImplMock;
+
+        sentenceValidatorImplMock = Mockito.mock(SentenceValidatorImpl.class);
+        doThrow(new SyntaxHighlightingException(""))
+                .when(sentenceValidatorImplMock).validate(null);
+        doThrow(new SyntaxHighlightingException(""))
+                .when(sentenceValidatorImplMock).validate("");
+        doThrow(new SyntaxHighlightingException(""))
+                .when(sentenceValidatorImplMock).validate("     ");
+        return sentenceValidatorImplMock;
     }
 
     @Test(expected=SyntaxHighlightingException.class)
