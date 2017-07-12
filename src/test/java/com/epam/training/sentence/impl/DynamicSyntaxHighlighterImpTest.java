e1a4a92e7f6bebc4a6d4b97725a2bb8e86dce902
Philip John Ardley <philip_john_ardley@epcnszxw0357.princeton.epam.com>
2017-07-12 12:44:14 +0800

    Failing test "shouldApplyStyleOnThisSentence" applied no style change.

--- src/test/java/com/epam/training/sentence/impl/DynamicSyntaxHighlighterImpTest.java
+++ src/test/java/com/epam/training/sentence/impl/DynamicSyntaxHighlighterImpTest.java
@@ -3,6 +3,7 @@ package com.epam.training.sentence.impl;
 import com.epam.training.exception.SyntaxHighlightingException;
 import com.epam.training.sentence.data.KeywordEffectPair;
 import com.epam.training.validation.impl.SentenceValidatorImp;
+import com.epam.training.word.impl.DynamicWordHighlighter;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
@@ -32,22 +33,11 @@ public class DynamicSyntaxHighlighterImpTest {
     public void setupSyntaxHighlighter() {
         dynamicSyntaxHighlighterImp = new DynamicSyntaxHighlighterImp(buildSentenceValidator());
         dynamicSyntaxHighlighterImp.setHighlightConfig(buildHighlightConfig());
+        dynamicSyntaxHighlighterImp.setDynamicWordHighlighter(buildWordHighlighter());
 
 
         noWordsSyntaxHighlighterImp = new DynamicSyntaxHighlighterImp(buildSentenceValidator());
         noWordsSyntaxHighlighterImp.setHighlightConfig(new ArrayList<>());
-    }
-
-    private Collection<KeywordEffectPair> buildHighlightConfig() {
-        String serializedConfig = "am-bold, to-italic, in-underline, to-yellow, java-red";
-        Iterator<String> words = Arrays.asList(serializedConfig.split("(-|,\\s?)")).iterator();
-        while(words.hasNext()) {
-            KeywordEffectPair thisMock = Mockito.mock(KeywordEffectPair.class);
-            when(thisMock.getKeyword()).thenReturn(words.next());
-            when(thisMock.getEffect()).thenReturn(words.next());
-            highlightConfigs.add(thisMock);
-        }
-        return highlightConfigs;
     }
 
     private SentenceValidatorImp buildSentenceValidator() {
@@ -63,6 +53,21 @@ public class DynamicSyntaxHighlighterImpTest {
         doThrow(throwable)
                 .when(mock).validate(input);
         return mock;
+    }
+
+    private Collection<KeywordEffectPair> buildHighlightConfig() {
+        String serializedConfig = "am-bold,to-italic,in-underline,to-yellow,java-red";
+        ArrayList<KeywordEffectPair> config = new ArrayList<>();
+        
+        Iterator<String> words = Arrays.asList(serializedConfig.split("(-|,\\s?)")).iterator();
+        while(words.hasNext()) {
+            config.add(new KeywordEffectPair(words.next(), words.next()));
+        }
+        return config;
+    }
+
+    private DynamicWordHighlighter buildWordHighlighter() {
+        return new DynamicWordHighlighter(new KeywordEffectPair("in", "underline"));
     }
 
     @Test(expected = SyntaxHighlightingException.class)
@@ -82,6 +87,24 @@ public class DynamicSyntaxHighlighterImpTest {
         dynamicSyntaxHighlighterImp.highlightThis(sentence);
     }
 
+    @Test
+    public void shouldNotApplyStyleOnThisSentence() {
+        String sentence = "I know everything, so it's waste of effort.";
+        String expected = "I know everything, so it's waste of effort.";
+
+        String highlightSentence = dynamicSyntaxHighlighterImp.highlightThis(sentence);
+        assertEquals(expected, highlightSentence);
+    }
+
+    @Test
+    public void shouldApplyStyleOnThisSentence() {
+        String sentence = "I am going to join,java mentoring program to learn cool stuff in fun way.";
+        String expected  = "I [bold] am [/bold] going [italic] [yellow] to [/yellow] [/italic] join,java mentoring program " +
+                "[italic] [yellow] to [/yellow] [/italic] learn cool stuff [underline] in [/underline] fun way.";
+
+        String highlightSentence = dynamicSyntaxHighlighterImp.highlightThis(sentence);
+        assertEquals(expected, highlightSentence);
+    }
 
     @Test
     public void shouldNotApplyStylesWhenNoWordsHighlighterAndThisSentence() {
